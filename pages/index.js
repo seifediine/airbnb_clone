7d90c66fe321a0bd2f5a8cc87f440dd33bae8e58
seifediine <seifeddine.elkorichi@hotmail.com>
2021-08-31 13:10:11 +0100

    adding large card

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard'
 import MediumCard from '../components/MediumCard'
+import LargeCard from '../components/LargeCard'
 
 const Home = ({ exploreData, cardsData }) => {
   return (
@@ -40,6 +41,12 @@ const Home = ({ exploreData, cardsData }) => {
             ))}
           </div>
         </section>
+
+        <LargeCard
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          buttonText='Get Inspired'
+        />
       </main>
     </div>
   )
